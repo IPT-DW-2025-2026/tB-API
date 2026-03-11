76263b68104b1163e789f112df166232d056aae9
Joao Silva <jotal96@gmail.com>
2026-03-11 12:05:33 +0000

    adicionada anotação displayName que altera o nome da coluna apresentada ao utilizador

--- tB-Fotografias/Models/Category.cs
+++ tB-Fotografias/Models/Category.cs
@@ -15,6 +15,7 @@ namespace tB_Fotografias.Models
         /// <summary>
         /// Nome da Categoria
         /// </summary> 
+        [Display(Name = "Nome da Categoria")]  // the name to show on screen
         [Required(ErrorMessage = "A {0} é de preenchimento obrigatório.")]
         [StringLength(32, ErrorMessage = "A {0} deve ter um máximo de {1} caracteres.")]
         public string Name { get; set; } = string.Empty;
