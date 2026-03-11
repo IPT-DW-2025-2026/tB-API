a704342f9e63bbf9e5e149149a7dd019e179b557
Joao Silva <jotal96@gmail.com>
2026-03-11 09:51:18 +0000

    commit inicial do projeto

---
+++ tB-Fotografias/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tB_Fotografias.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
